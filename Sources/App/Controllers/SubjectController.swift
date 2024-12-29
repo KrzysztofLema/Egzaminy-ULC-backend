526dffe75e8c24a4bbea170439fc50aad39265ff
Krzysztof Lema <lema10@o2.pl>
2024-12-29 16:42:39 +0100

    add examID param to endpoint

--- Sources/App/Controllers/SubjectController.swift
+++ Sources/App/Controllers/SubjectController.swift
@@ -5,15 +5,17 @@ struct SubjectController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let subjectsRoutes = routes.grouped("api", "subjects")
         
-        subjectsRoutes.get(":examID", use: getAllSubjectsByExamIDHandler)
+
+        subjectsRoutes.get("", use: getAllSubjectsByExamIDHandler)
     }
-    
+
     @Sendable
     func getAllSubjectsByExamIDHandler(_ req: Request) async throws -> [Subject] {
-        guard let examID = req.parameters.get("examID", as: UUID.self) else {
-            throw Abort(.badRequest, reason: "Invalid exam ID")
+        guard let examIDString = req.query[String.self, at: "examID"],
+              let examID = UUID(uuidString: examIDString) else {
+            throw Abort(.badRequest, reason: "Invalid or missing examID query parameter")
         }
-        
+
         return try await Subject.query(on: req.db)
             .filter(\.$exam.$id == examID)
             .all()
